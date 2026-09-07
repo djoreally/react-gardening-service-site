fdbd509dca6dd7058e77c99c369ec676d9ada127
Tyreese Burton <djoreally@gmail.com>
2026-09-07 16:22:58 -0400

    BuildOS repair: make matchMedia legacy compatible

--- src/setupTests.js
+++ src/setupTests.js
@@ -1,15 +1,15 @@
 import '@testing-library/jest-dom';
 
-Object.defineProperty(window, 'matchMedia', {
-  writable: true,
-  value: jest.fn().mockImplementation(query => ({
-    matches: false,
-    media: query,
-    onchange: null,
-    addListener: jest.fn(),
-    removeListener: jest.fn(),
-    addEventListener: jest.fn(),
-    removeEventListener: jest.fn(),
-    dispatchEvent: jest.fn(),
-  })),
+const createMediaQueryList = query => ({
+  matches: false,
+  media: query,
+  onchange: null,
+  addListener: jest.fn(),
+  removeListener: jest.fn(),
+  addEventListener: jest.fn(),
+  removeEventListener: jest.fn(),
+  dispatchEvent: jest.fn(),
 });
+
+window.matchMedia = query => createMediaQueryList(query);
+global.matchMedia = window.matchMedia;
